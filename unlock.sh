9f034a2c1ec0598cbcacf14c230e499a9046e8e7
Mohamed Osama <mohamed.o.alnagdy@gmail.com>
2015-10-03 23:28:47 +0200

    Xperia Z1

--- unlock.sh
+++ unlock.sh
@@ -30,22 +30,22 @@
 # 4 5 6
 # 7 8 9
 
-PATTERN="1 2 3 6 5 4 7 8 9" # The unlock pattern to draw, space seperated
+PATTERN="1 2 3 4 5 6 7 8 9" # The unlock pattern to draw, space seperated
 
-COL_1=166                   # X coordinate of column 1 (in pixels)
-COL_2=384                   # X coordinate of column 2 (in pixels)
-COL_3=576                   # X coordinate of column 3 (in pixels)
+COL_1=234                   # X coordinate of column 1 (in pixels)
+COL_2=538                   # X coordinate of column 2 (in pixels)
+COL_3=842                   # X coordinate of column 3 (in pixels)
 
-ROW_1=553                   # Y coordinate of row 1 (in pixels)
-ROW_2=738                   # Y coordinate of row 2 (in pixels)
-ROW_3=984                   # Y coordinate of row 3 (in pixels)
+ROW_1=781                   # Y coordinate of row 1 (in pixels)
+ROW_2=1085                  # Y coordinate of row 2 (in pixels)
+ROW_3=1380                   # Y coordinate of row 3 (in pixels)
 
-MULTIPLIER=2                # Multiplication factor for coordinates. For Nexus 4, set this to 2. For low res phones such as
+MULTIPLIER=1                # Multiplication factor for coordinates. For Nexus 4, set this to 2. For low res phones such as
                             # Samsung Galaxy S2, set this to 1. Experiment with this value if you can't see anything happening.
 
-WAKE_SCREEN_ENABLED=true    # If true, the script will start by sending the power button press event
+WAKE_SCREEN_ENABLED=false    # If true, the script will start by sending the power button press event
 
-SWIPE_UP_ENABLED=true       # If true, the script will swipe upwards before drawing the pattern (e.g. for lollipop lockscreen)
+SWIPE_UP_ENABLED=false       # If true, the script will swipe upwards before drawing the pattern (e.g. for lollipop lockscreen)
 SWIPE_UP_X=450              # X coordinate for initial upward swipe. Only used if SWIPE_UP_ENABLED is true
 SWIPE_UP_Y_FROM=1000        # Start Y coordinate for initial upward swipe. Only used if SWIPE_UP_ENABLED is true
 SWIPE_UP_Y_TO=200           # End Y coordinate for initial upward swipe. Only used if SWIPE_UP_ENABLED is true
